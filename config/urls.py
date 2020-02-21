4e74ca07e01cf6ea00e7fce2b8df74ee86bc3644
hidalgopl <pawebojanowski@gmail.com>
2020-02-21 21:02:24 +0100

    remove chart for now, update swagger metadata

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="SecureAPI REST API",
         default_version="v1",
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
+        description="List of endpoint",
+        terms_of_service="https://www.secureapi.dev/terms/",
+        contact=openapi.Contact(email="pawebojanowski@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
